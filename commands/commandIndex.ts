422e4a154806f7456d81b6843535115e5ff5a36e
LokiLP66 <68025706+LokiLP66@users.noreply.github.com>
2022-07-05 16:33:11 +0200

    edit

--- commands/commandIndex.ts
+++ commands/commandIndex.ts
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 export * as ping from './ping'
 export * as ticket from './ticket'
-import { name, description } from './help'
 import fs from 'fs'
 import path from 'path'
 
 const commandsPath = path.join(__dirname, '../commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'))
 
+
 for (let i = 1; i < commandFiles.length; i++) {
-    const text = commandFiles[i].indexOf('.ts')
-    const len = commandFiles[i].length
-    const cmd = commandFiles[i].slice(text, len)
-    console.log(cmd)
-  }
+	const text = commandFiles[i].indexOf('.ts')
+	const cmd = commandFiles[i].slice(0, text)
+	const comds = {
+		name: `/${cmd}`,
+		description: `use /${cmd} for ${cmd}`
+	}
+
+	console.log(comds)
+}
