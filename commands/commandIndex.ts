283e27d983b68dcef6649db884c8e4bb2a61ab73
LokiLP66 <68025706+LokiLP66@users.noreply.github.com>
2022-07-05 18:53:52 +0200

    edit

--- commands/commandIndex.ts
+++ commands/commandIndex.ts
@@ -6,15 +6,15 @@ import path from 'path'
 
 const commandsPath = path.join(__dirname, '../commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'))
+commandFiles.shift()
+const cmdFiles = commandFiles
+const len = cmdFiles.length
 
-
-for (let i = 1; i < commandFiles.length; i++) {
+/* for (let i = 1; i < commandFiles.length; i++) {
 	const text = commandFiles[i].indexOf('.ts')
 	const cmd = commandFiles[i].slice(0, text)
 	const comds = {
 		name: `/${cmd}`,
 		description: `use /${cmd} for ${cmd}`
 	}
-
-	console.log(comds)
-}
+}*/
